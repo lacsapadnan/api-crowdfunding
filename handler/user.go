61eee83978c09d1507e860f0f93ae146d9021506
lacsapadnan <pascaladnan101@gmail.com>
2022-04-06 20:43:13 +0700

    Login API Endpoint

--- handler/user.go
+++ handler/user.go
@@ -44,3 +44,32 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 		
 	c.JSON(http.StatusOK, response)
 }
+
+func (h *userHandler) LoginUser(c *gin.Context) {
+	var input user.LoginUserInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helpers.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helpers.APIResponse("Login failed", http.StatusBadRequest, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return 
+	}
+
+	loggedinUser, err := h.userService.LoginUser(input)
+
+	if err != nil {
+		errorMessage := gin.H{"errors": err.Error()}
+
+		response := helpers.APIResponse("Login failed", http.StatusBadRequest, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return 
+	}
+
+	formatter := user.FormatUser(loggedinUser, "tokenJWTAuhtorization")
+	response := helpers.APIResponse("Login successfully", http.StatusOK, "success", formatter)
+		
+	c.JSON(http.StatusOK, response)
+}
